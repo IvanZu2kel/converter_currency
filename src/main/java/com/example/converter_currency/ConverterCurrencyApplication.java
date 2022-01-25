00efcea3024511ba3b3a53cad62938c60435b8c9
Ivan Zu2kel <ivan_zukkel@mail.ru>
2022-01-25 23:02:54 +0300

    Initial commit

---
+++ src/main/java/com/example/converter_currency/ConverterCurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.example.converter_currency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConverterCurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConverterCurrencyApplication.class, args);
+    }
+
+}
