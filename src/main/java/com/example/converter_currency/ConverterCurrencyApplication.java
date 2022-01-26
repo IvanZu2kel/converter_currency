9119dc1a2d8c13e4af392b1890fa36314fa84c9a
Ivan Zu2kel <ivan_zukkel@mail.ru>
2022-01-26 08:18:08 +0300

    Инициализация информации с цб

--- src/main/java/com/example/converter_currency/ConverterCurrencyApplication.java
+++ src/main/java/com/example/converter_currency/ConverterCurrencyApplication.java
@@ -1,7 +1,11 @@
 package com.example.converter_currency;
 
+import com.example.converter_currency.repositories.CurrencyRateRepository;
+import com.example.converter_currency.repositories.CurrencyRepository;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ConverterCurrencyApplication {
@@ -10,4 +14,8 @@ public class ConverterCurrencyApplication {
         SpringApplication.run(ConverterCurrencyApplication.class, args);
     }
 
+    @Bean
+    ApplicationRunner init(CurrencyRateRepository currencyRateRepository, CurrencyRepository currencyRepository) {
+        return null;
+    }
 }
